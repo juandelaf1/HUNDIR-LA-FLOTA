80a985b8fe8b6915515634cb2a93ed8e7e91e0a9
juandelaf1 <juandelafuentelarrocca@gmail.com>
2025-03-17 09:15:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 
 import utils
 
-tablero1 = crear_tj1()
-tablero2 = crear_tj2()
-barcoj1 = crear_barco_aleatorioj1()
-barcoj2 = crear_barco_aleatorioj2()
-flotaj1 = crear_flotaj1()
-flotaj2 = crear_flotaj2()
-tablero1 =colocar_barcoj1()
-tablero2 = colocar_barcoj2()
-tablero1 = colocar_flotaj1()
-tablero2 = colocar_flotaj2()
-tab_1_shoot = dispararj1()
-tab_2_shoot = dispararj2()
+tab_jug1, tab_1_shoot = u.crear_tj1()
+tab_jug2, tab_2_shoot = u.crear_tj2()
+barcoj1 = u.crear_barco_aleatorioj1()
+barcoj2 = u.crear_barco_aleatorioj2()
+flotaj1 = u.crear_flotaj1()
+flotaj2 = u.crear_flotaj2()
+
+tablero1 = u.colocar_flotaj1(flotaj1, tab_jug1 )
+tablero2 = u.colocar_flotaj2(flotaj2, tab_jug2)
+tab_1_shoot = u.dispararj1()
+tab_2_shoot = u.dispararj2()
 
 # BUCLE PARA INICIAR EL JUEGO
 
 while True:
         ingreso = input("Bienvenid@s a HUNDIR LA FLOTA. Marca 1 para comenzar a jugar")
         if ingreso!="":
-                crear_tj1()
-                colocar_flotaj1()
+                u.crear_tj1()
+                u.colocar_flotaj1()
                 print("Estos son tus tableros \n")
-                crear_tj2()
-                colocar_flotaj2()          
+                u.crear_tj2()
+                u.colocar_flotaj2()          
         break
 # BUCLE PARA DETERMINAR DURACION DEL JUEGO
 while True:
